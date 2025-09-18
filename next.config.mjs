eee9443e949eb59217c884a5395b1f74c57c5fb7
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-09-19 02:01:00 +0900

    許可ドメインを増加

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: ["firebasestorage.googleapis.com"],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "openhacku2025.firebasestorage.app",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
